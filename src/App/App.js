29546d1c4c8dfd6e64e1641c9702942733c0c2aa
Jeressia Williamson <jeressiajaytv@gmail.com>
2019-06-18 20:47:11 -0500

    setup complete

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
 
 function App() {
   return (
@@ -16,6 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
+          <button className='btn btn-danger'>HELP ME</button>
           Learn React
         </a>
       </header>
